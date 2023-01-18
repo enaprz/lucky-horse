4c109cbad7c2a2b51df33782c06b3fa36492a8a5
enaprz <enasheccid7@gmail.com>
2023-01-17 21:38:21 -0800

    blue print, new files

---
+++ .js
@@ -0,0 +1,22 @@
+// choose bet amount
+
+
+// choose horse
+
+
+
+//begin race
+
+
+
+// function to generate random winner
+
+
+
+//display winner
+
+
+//all out of money display
+
+
+//double bet money if winner 
